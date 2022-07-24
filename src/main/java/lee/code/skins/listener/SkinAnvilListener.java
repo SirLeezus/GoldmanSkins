22b4a1afb37c33e2625184a68554a1be1da9566e
Lee <leecook808@gmail.com>
2022-07-24 01:32:56 -0700

    Anvil fix

--- src/main/java/lee/code/skins/listener/SkinAnvilListener.java
+++ src/main/java/lee/code/skins/listener/SkinAnvilListener.java
@@ -5,7 +5,10 @@ import lee.code.skins.lists.Skin;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
+import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -30,6 +33,22 @@ public class SkinAnvilListener implements Listener {
                         result.setItemMeta(resultMeta);
                         e.setResult(result);
                         if (e.getInventory().getRepairCost() < 1) Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> e.getInventory().setRepairCost(1), 1L);
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onAnvilRenameDeny(InventoryClickEvent e) {
+        if (e.getInventory() instanceof AnvilInventory anvilInventory) {
+            if (e.getSlotType() != InventoryType.SlotType.RESULT) return;
+            ItemStack[] contents = anvilInventory.getContents();
+            ItemStack firstSlot = contents[0];
+            if (firstSlot!= null) {
+                if (firstSlot.getItemMeta().hasCustomModelData()) {
+                    if (firstSlot.getItemMeta().getCustomModelData() == 4000) {
+                        e.setCancelled(true);
                     }
                 }
             }
